c98f7ca74973a9a61b80b284d7f270745e4fef83
Ali Iftikhar <20011519-054@uog.edu.pk>
2024-08-21 23:54:29 -0700

    initial commit

--- src/app/api/company/route.js
+++ src/app/api/company/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import prisma from '../../lib/prisma';
-
+import { PrismaClient } from "@prisma/client";
+let prisma = new PrismaClient();
 export async function POST(request) {
   try {
     const data = await request.json();
